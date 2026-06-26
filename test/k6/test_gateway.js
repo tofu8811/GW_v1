7b5ea0d3cc982d85eae8fd93729fb57b356c3681
phuong hieu <phuonghieuphan666@gmail.com>
2026-06-27 01:45:27 +0700

    lb: round-robin strategy is settled

---
+++ test/k6/test_gateway.js
@@ -0,0 +1,41 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+const BASE_URL = 'http://127.0.0.1:8080';
+const TOKEN = 'Bearer YOUR_TOKEN';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 20 },  // tăng lên 20 users
+    // { duration: '30s', target: 50 },  // giữ 50 users
+    { duration: '10s', target: 0 },  // giảm về 0
+  ],
+};
+
+// Mỗi user sẽ chạy function này lặp đi lặp lại
+export default function () {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': TOKEN,
+  };
+
+  // Gửi đồng thời nhiều endpoint trong 1 vòng lặp
+  const responses = http.batch([
+    ['GET', `${BASE_URL}/api/products`, null, { headers }],
+    ['GET', `${BASE_URL}/api/product/5`, null, { headers }],
+    ['GET', `${BASE_URL}/api/product/7`, null, { headers }],
+    // ['GET', `http://localhost:3001/api/products`, null, { headers }],
+    // ['GET', `http://localhost:3003/api/products`, null, { headers }],
+    // ['GET', `http://localhost:3004/api/products`, null, { headers }],
+    // ['GET', `http://localhost:3005/api/products`, null, { headers }],
+  ]);
+
+  // Check từng response
+  check(responses[0], { '[gateway] GET products 200': (r) => r.status === 200 });
+  check(responses[1, 2], { '[gateway] GET product/{id} 200': (r) => r.status === 200 });
+  // check(responses[0], { '[service:3001] GET products 200': (r) => r.status === 200 });
+  // check(responses[1], { '[service:3003] GET products 200': (r) => r.status === 200 });
+  // check(responses[2], { '[service:3004] GET products 200': (r) => r.status === 200 });
+  // check(responses[3], { '[service:3005] GET products 200': (r) => r.status === 200 });
+  sleep(1);
+}
